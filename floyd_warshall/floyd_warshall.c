2dd34b6053a9a00fcb1bd7a7af9369c5ac8a2df0
paodealho404 <phdbnpedro@gmail.com>
2022-02-20 21:22:23 -0300

    feat: Lidando com os parâmetros solicitados na atividade

--- floyd_warshall/floyd_warshall.c
+++ floyd_warshall/floyd_warshall.c
@@ -2,19 +2,68 @@
 #include "stdio.h"
 #include <stdlib.h>
 #include <string.h>
-void menu_fw(void){
-    int v, a;
-    scanf("%d %d", &v, &a);
-    int grafos[v+1][v+1];
-    memset(grafos, 0, sizeof(grafos));
-    for (int i = 0; i < a; i++)
-    {
-        int v1, v2, p;
-        scanf("%d %d %d", &v1, &v2, &p);
-        grafos[v1][v2] = p;
+#include <limits.h>
+#include <unistd.h> 
+void print_dist_matrix(int v, int graph[v][v]){
+    for (int i = 1; i < v; i++){
+        for (int j = 1; j < v; j++)
+            printf("| %d |", graph[i][j]);
+        printf("\n");
     }
-
+        
 }
-void main(){
-    printf("Hello FW");
+void initialize(int v, int graph[v][v]){
+    for (int i = 1; i < v; i++)
+        for (int j = 1; j < v; j++)
+            graph[i][j]= INT_MAX;
+}
+void fill(int v, int a, int graph[v][v]){
+
+    for (int i = 0; i < a; i++)
+        {
+            int v1, v2, p;
+            scanf("%d %d %d", &v1, &v2, &p);
+            graph[v1][v2] = p;
+        }   
+    }
+void main(int argc, char *argv[]){
+    int opt;
+    while((opt = getopt(argc, argv, "ho:f:l:si:l:")) != -1){ 
+        switch(opt) 
+        { 
+            case 'h':
+                printf("Help");
+                break;
+            case 's': 
+                printf("Sorted solution");
+                break;
+            case 'l': 
+                printf("Target %d", opt);
+                break;
+            case 'i': 
+                printf("Source %d", opt);
+                break;
+            case 'o': 
+                printf("Output %s", optarg); 
+                break; 
+            case 'f': 
+                printf("Input %s", optarg); 
+                break; 
+            case ':': 
+                printf("Os parâmetros precisam de um valor\n"); 
+                break; 
+            case '?': 
+                printf("Parametro desconhecido: %c\n", opt);
+                break; 
+        } 
+    } 
+    // int v, a;
+    // fscanf("%d %d", &v, &a);
+    // int grafos[v+1][v+1];
+    // FILE *input_file, *output_file;
+
+    // initialize(v+1,grafos);
+
+    // print_dist_matrix(v+1, grafos);
+    
 }
